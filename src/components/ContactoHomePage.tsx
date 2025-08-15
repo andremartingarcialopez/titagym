fcabcb5535c611b76ad23a08995a567fc55a2227
Andre <andy_garlop4@hotmail.com>
2025-08-14 21:32:11 -0600

    Se valido el formulario

--- src/components/ContactoHomePage.tsx
+++ src/components/ContactoHomePage.tsx
@@ -1,6 +1,95 @@
+import { useState } from "react";
 import Footer from "./Footer";
+import { Slide, toast, } from "react-toastify"
 
 export default function ContactoHomePage() {
+
+    const [form, setForm] = useState({
+        name: "",
+        age: "",
+        phone: "",
+        email: ""
+    });
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setForm({
+            ...form,
+            [e.target.id]: e.target.value
+        })
+    }
+
+    function validateEmail(email: string) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+        const result = regex.test(email);
+        return result
+    }
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        if (Object.values(form).some(value => value.trim() == "")) {
+            toast.error('Todos los campos son obligatorios', {
+                position: "top-center",
+                autoClose: 1500,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Slide,
+            });
+            return
+        }
+        if (!validateEmail(form.email)) {
+            toast.error('Email no valido', {
+                position: "top-center",
+                autoClose: 1500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Slide,
+            });
+            return
+        }
+
+        if (form.phone.length < 10) {
+            toast.error('Numero telefonico no valido', {
+                position: "top-center",
+                autoClose: 1500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Slide,
+            });
+            return
+        }
+
+        setForm({
+            name: "",
+            age: "",
+            phone: "",
+            email: ""
+        })
+        toast.success('¡Te contactaremos pronto!', {
+            position: "top-center",
+            autoClose: 1500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Slide,
+
+        });
+    }
+
     return (
         <>
             <div className="bg-img-layout-homePage-contacto">
@@ -11,43 +100,56 @@ export default function ContactoHomePage() {
 
                         <h1 className="text-yellow-400 text-4xl md:text-8xl font-bold uppercase border-b-4 pb-3 text-center xl:text-left">Ponte en <span className="xl:block">contacto</span></h1>
 
-                        <form className="bg-white/90 rounded-xl p-5 w-full xl:w-100 flex flex-col justify-center items-stretch space-y-3">
+                        <form className="bg-white/90 rounded-xl p-5 w-full xl:w-100 flex flex-col justify-center items-stretch space-y-3"
+                            onSubmit={handleSubmit}>
                             <div>
                                 <label className="text-gray-800 "
                                     htmlFor="name">Nombre</label>
                                 <input className="w-full outline-amber-400 rounded-lg p-2 bg-gray-400/30 text-sm"
                                     type="text"
-                                    placeholder="Ej. Andy" />
+                                    placeholder="Ej. Andy"
+                                    id="name"
+                                    value={form.name}
+                                    onChange={handleChange} />
                             </div>
 
                             <div>
                                 <label className="text-gray-800 "
-                                    htmlFor="name">Edad</label>
+                                    htmlFor="age">Edad</label>
                                 <input className="w-full outline-amber-400 rounded-lg p-2 bg-gray-400/30 text-sm"
                                     type="number"
-                                    placeholder="Ej. 27" />
+                                    placeholder="Ej. 27"
+                                    id="age"
+                                    value={form.age}
+                                    onChange={handleChange} />
                             </div>
 
                             <div>
                                 <label className="text-gray-800 "
-                                    htmlFor="name">Telefono</label>
+                                    htmlFor="phone">Telefono</label>
                                 <input className="w-full outline-amber-400 rounded-lg p-2 bg-gray-400/30 text-sm"
                                     type="tel"
                                     maxLength={10}
-                                    placeholder="Ej. 3312839670" />
+                                    placeholder="Ej. 3312839670"
+                                    id="phone"
+                                    value={form.phone}
+                                    onChange={handleChange} />
                             </div>
 
                             <div>
                                 <label className="text-gray-800 "
-                                    htmlFor="name">Email</label>
+                                    htmlFor="email">Email</label>
                                 <input className="w-full outline-amber-400 rounded-lg p-2 bg-gray-400/30 text-sm"
                                     type="email"
-                                    placeholder="Ej. user@correo.com" />
+                                    placeholder="Ej. user@correo.com"
+                                    id="email"
+                                    value={form.email}
+                                    onChange={handleChange} />
                             </div>
 
-                            <input className="bg-yellow-400 p-2 rounded-lg uppercase hover:bg-yellow-500 cursor-pointer hover:font-semibold active:bg-yellow-400 mt-3 w-full lg:w-50" 
-                            value={"Contactame"} 
-                            type="submit" />
+                            <input className="bg-yellow-400 p-2 rounded-lg uppercase hover:bg-yellow-500 cursor-pointer hover:font-semibold active:bg-yellow-400 mt-3 w-full lg:w-50"
+                                value={"Contactame"}
+                                type="submit" />
                         </form>
                     </div>
                 </div>
--- src/components/ServicesImages.tsx
+++ src/components/ServicesImages.tsx
@@ -11,9 +11,10 @@ export default function ServicesImages({title,image}: ServicesImgesProps) {
             {/* Overlay oscuro */}
             <div className="absolute inset-0 bg-black/40 group-hover:bg-black/60 transition duration-300"></div>
             {/* Texto centrado */}
-            <h2 className="absolute inset-0 flex items-center justify-center text-white text-2xl md:text-6xl xl:text-2xl font-bold uppercase hover:underline">
+            <h2 className="absolute inset-0 flex items-center justify-center text-white text-2xl md:text-6xl xl:text-2xl font-bold uppercase hover:underline text-center">
                 {title}
             </h2>
+            <p className="absolute inset-0 flex justify-center items-center mt-30 text-white">Mas Informacion</p>
         </a>
     )
 }
