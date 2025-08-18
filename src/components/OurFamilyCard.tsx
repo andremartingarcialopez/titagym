9837b513e768091797bc3c143f683032979754ab
Andre <andy_garlop4@hotmail.com>
2025-08-18 16:18:24 -0600

    Se creo la seccion de nuestros entrenadores

--- src/components/OurFamilyCard.tsx
+++ src/components/OurFamilyCard.tsx
@@ -1,25 +1,31 @@
 
 type OurFamilyCardProps = {
-  name:string
-  age:string
-  specialty:string
-  description:string
+  image: string
+  name: string
+  age: string
+  specialty: string
+  description: string
 }
 
-export default function OurFamilyCard({name,age,specialty,description} : OurFamilyCardProps) {
+export default function OurFamilyCard({ image, name, age, specialty, description }: OurFamilyCardProps) {
 
 
   return (
-      <div className="lg:w-100 shadow">
-        <img className=" rounded-t-lg shadow shadow-amber-400 p-5 bg-yellow-400 " src="https://img.freepik.com/foto-gratis/hombre-atletico-practicando-gimnasia-mantenerse-forma_23-2150989961.jpg" alt="Imagen de Felix" />
-        <div className="bg-yellow-400 shadow shadow-amber-400 px-3 pb-4 rounded-b-2xl flex flex-col justify-center items-stretch space-y-1">
-          <p className="font-semibold px-3">Nombre: <span className="font-light">{name}</span></p>
-          <p className="font-semibold px-3">Edad: <span className="font-light">{age}</span></p>
-          <p className="font-semibold px-3">Especialidad: <span className="font-light">{specialty}</span></p>
-          <p className="font-semibold px-3">Descripcion: <span className="font-light">{description}</span></p>
-
-        </div>
+    <div className=" flex flex-col md:flex-row bg-yellow-400 rounded-lg p-2 justify-center items-center">
+      
+      <div className="">
+        <p className="text-center md:text-left font-semibold py-2 text-xl md:text-3xl">{name}</p>
+        <img className=" rounded-lg bg-yellow-400 " src={image} alt="Imagen de Felix" />
       </div>
-    
+
+      <div className="py-2 md:text-xl">
+        <p className="font-semibold px-3">Edad: <span className="font-light">{age}</span></p>
+        <p className="font-semibold px-3">Especialidad: <span className="font-light">{specialty}</span></p>
+        <p className="font-semibold px-3">Descripcion: <span className="font-light">{description}</span></p>
+      </div>
+
+    </div>
+
+
   )
 }
--- src/pages/OurFamilyPage.tsx
+++ src/pages/OurFamilyPage.tsx
@@ -6,14 +6,45 @@ export default function OurFamilyPage() {
     <>
       <HeroImage bgImage="bg-ourFamily" title="Nuestra Familia" />
 
-      <div className="bg-black" id="users">
+      <div className="bg-black">
 
-        <div className=" grid grid-cols-1  justify-center items-center mx-auto container">
+        <div className=" grid grid-cols-1 justify-center items-center mx-auto container">
           <div className="mx-5 xl:m-0">
-            <h2 className="text-yellow-400 text-5xl font-semibold text-center py-10 xl:my-0">Nutros Entrenadores</h2>
+            <h2 className="text-yellow-400 text-5xl font-semibold text-center py-10 xl:my-0">Nuestros Entrenadores</h2>
           </div>
-          <div className="flex flex-col lg:flex-row justify-center items-center gap-10 xl:gap-5 m-3">
-            <OurFamilyCard name="Felix Pakers" age="35" specialty="Halterofilia y powerlifting" description="Campeón mundial en levantamiento de pesas estilo libre, con más de 12 años de experiencia entrenando atletas de alto rendimiento. Su enfoque está en la fuerza, la técnica y la disciplina"  />
+
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mb-5 mx-2">
+            <OurFamilyCard
+              image="https://img.freepik.com/foto-gratis/hombre-atletico-practicando-gimnasia-mantenerse-forma_23-2150989961.jpg"
+              name="Felix Pakers"
+              age="35"
+              specialty="Halterofilia y powerlifting"
+              description="Campeón mundial en levantamiento de pesas estilo libre, con más de 12 años de experiencia entrenando atletas de alto rendimiento. Su enfoque está en la fuerza, la técnica y la disciplina."
+            />
+
+            <OurFamilyCard
+              image="https://img.freepik.com/foto-gratis/mujer-atletica-haciendo-ejercicio-gimnasio-mantenerse-forma_23-2150989815.jpg"
+              name="Jasmin Lopez"
+              age="29"
+              specialty="Programas de alta intensidad (HIIT) y resistencia."
+              description="Certificada en CrossFit Level 2, ha participado en competencias nacionales de fitness funcional. Su método busca mejorar agilidad, coordinación y resistencia cardiovascular."
+            />
+
+            <OurFamilyCard
+              image="https://img.freepik.com/foto-gratis/hombre-atletico-practicando-gimnasia-mantenerse-forma_23-2150989957.jpg"
+              name="Alexander Ruiz"
+              age="30"
+              specialty="Yoga integral, respiración y movilidad"
+              description="Instructor internacional de yoga con certificación RYT-500, experto en meditación guiada y control de estrés. Ayuda a sus alumnos a alcanzar un equilibrio físico y mental."
+            />
+
+            <OurFamilyCard
+              image="https://img.freepik.com/foto-gratis/mujer-atletica-haciendo-ejercicio-gimnasio-mantenerse-forma_23-2150989969.jpg"
+              name="Sofia Martinez"
+              age="31"
+              specialty="Boxeo, kickboxing y defensa personal."
+              description="Excompetidora de artes marciales mixtas con trayectoria en torneos internacionales. Su entrenamiento combina fuerza explosiva, velocidad y confianza personal."
+            />
           </div>
         </div>
       </div>
